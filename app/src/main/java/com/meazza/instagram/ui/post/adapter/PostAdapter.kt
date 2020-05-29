15cbfafdfbcbad1fef7ef2309d4dcf6dd422f044
chris_meazza <christianmeazza@gmail.com>
2020-05-29 14:20:23 -0500

    PostAdapter

---
+++ app/src/main/java/com/meazza/instagram/ui/post/adapter/PostAdapter.kt
@@ -0,0 +1,40 @@
+package com.meazza.instagram.ui.post.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.paging.FirestorePagingAdapter
+import com.firebase.ui.firestore.paging.FirestorePagingOptions
+import com.meazza.instagram.R
+import com.meazza.instagram.common.listener.OnPostClickListener
+import com.meazza.instagram.data.model.Post
+import com.meazza.instagram.databinding.LayoutPostBinding
+
+class PostAdapter(
+    options: FirestorePagingOptions<Post>,
+    private val listener: OnPostClickListener
+) :
+    FirestorePagingAdapter<Post, PostAdapter.ExploreViewHolder>(options) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ExploreViewHolder(
+        DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.layout_post,
+            parent,
+            false
+        )
+    )
+
+    override fun onBindViewHolder(holder: ExploreViewHolder, position: Int, model: Post) {
+        holder.itemBinding.run {
+            post = model
+            root.setOnClickListener {
+                listener.onClickPost(model)
+            }
+        }
+    }
+
+    inner class ExploreViewHolder(val itemBinding: LayoutPostBinding) :
+        RecyclerView.ViewHolder(itemBinding.root)
+}
